983f0e6f9e6b4079af9163081809daa6aaae592d
Raghu K <raghuk.vit@gmail.com>
2022-09-15 06:38:58 +0530

    Variables

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,6 +1,6 @@
-a=10
-b=abc
-c=true
+a=10    # integer
+b=abc   # string
+c=true  # boolean
 
 echo Value of a = $a
 
@@ -11,3 +11,6 @@ echo Today Date is $DATE
 # Arthimetic Subs
 ADD=$((2+3+4))
 echo Addition of 2+3+4 is $ADD
+
+## Variable from CLI
+echo Value of x = $x
