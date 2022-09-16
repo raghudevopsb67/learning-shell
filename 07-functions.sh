062f096aed97f84cb3dbd7648534343209432acb
Raghu K <raghuk.vit@gmail.com>
2022-09-16 07:05:22 +0530

    Update learning-shell/

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,7 +1,12 @@
 ## Declaration way 1
 
 function SAMPLE() {
+  echo a from Main Program = $a
+  a=100
+  echo a in Function after Override in function - $a
   echo SAMPLE FUNCTION
+  b=10
+  echo First Argument to Script = $1
 }
 
 
@@ -10,5 +15,11 @@ SAMPLE1() {
   echo SAMPLE1 FUNCTION
 }
 
+
+## Main Program
+a=10
 SAMPLE
-SAMPLE1
+#SAMPLE1
+echo b from Function = $b
+b=200
+echo b from function after override in Main program -  $b
